6f79bc62d7943d73b8e226cf2612f2743b75a69d
Wills Grave <will@keydigitalconsulting.com>
2019-12-21 10:07:23 -0500

    Rounded out images and links.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const style = {
   logoIcon: {
-    width: 150,
+    width: 100,
     heigth: 100
   },
   bgDark: {
@@ -12,7 +12,7 @@ const style = {
 
 const Banner = props => (
   <article style={style.bgDark} className="mw7 center ph3 ph5-ns tc br2 pv3 bg-black-90 mb5 animated fadeIn">
-    <img style={style.logoIcon} className='logoIcon' src="./img/pacman.gif" alt="pacman"/>
+    <img style={style.logoIcon} className='logoIcon' src="./img/horo.png" alt="horoscope"/>
     <h2 className="fw2 f4 lh-copy mt0 mb3 white animated fadeIn slower">
     Click on an image to earn points, but don't click on any more than once!
     </h2>
